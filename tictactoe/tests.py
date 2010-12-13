3a188d31358584a43a03966c24a1959327ff3737
emidln <emidln@gmail.com>
2010-12-13 11:26:23 -0600

    Added remainder of tests for logic and basic web tests

--- tictactoe/tests.py
+++ tictactoe/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test.client import Client
 from models import Board
 from logic import evaluate_gamestate as eg
 from logic import win, block, corner, first, win_block_corner_first
@@ -71,8 +72,63 @@ class LogicTests(TestCase):
             self.assertEqual(eg(self.b), True)
             self.assertEqual(bool([x for x in range(len(self.b)) if self.b[x] == COMPUTER if x != 4 if x in corners]), True)
 
-    def test_move_3(self):
-        pass
+    def test_move_3_opp_center(self):
+        def t(f):
+            self.reset() 
+            r = range(9)
+            r.pop(4)
+            y = r[:]
+            for x in r:
+                for z in y:
+                    if x != z:
+                        self.b.squares_l = '000020000'
+                        self.b[x] = 1
+                        self.b[z] = 2
+                        self.assertEqual(eg(self.b), True)
+        t(win_block_corner_first)
+        t(eg)
+
+    def test_move_3_me_corners_opp(self):
+        # opp in opposite corners
+        self.reset()
+        self.b.squares_l = '200010002'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(self.b[1], COMPUTER)
+
+    def test_move_3_me_corners_not_opp(self):
+        # opp in corners not opposite
+        self.reset()
+        self.b.squares_l = '202010000'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(self.b[1], COMPUTER)
+
+    def test_move_3_me_one_side(self):
+        # opp in one side forcing block
+        self.reset()
+        self.b.squares_l = '220010000'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(self.b[2], COMPUTER)
+        
+        # opp doesn't force block
+        self.reset()
+        self.b.squares_l = '200012000'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(bool([x for x in self.b.taken() if self.b[x] == COMPUTER if x != 4 if x in [0,2,6,8]]), True)
+        
+
+    def test_move_3_me_two_sides(self):
+        # opp in both sides
+        self.reset()
+        self.b.squares_l = '020012000'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(self.b[2], COMPUTER)
+    
+    def test_move_3_me_two_sides_opp(self):
+        # opp in two sides, opposite
+        self.reset()
+        self.b.squares_l = '020010020'
+        self.assertEqual(eg(self.b), True)
+        self.assertEqual(bool([x for x in self.b.taken() if self.b[x] == COMPUTER if x != 4 if x in [0,2,6,8]]), True)
 
     def test_win(self):
         def t(f):
@@ -99,7 +155,43 @@ class LogicTests(TestCase):
         self.assertEqual(self.b[5], COMPUTER)
 
     def test_corner(self):
-        pass
+        def t(f):
+            self.reset()
+            self.b.squares_l = '100020002'
+            self.assertEqual(f(self.b), True)
+            self.assertEqual(len([x for x in self.b.taken() if self.b[x] == COMPUTER if x in [0,2,6,8]]), 2) 
+        t(corner)
+        t(eg)
 
-    def test_any(self):
-        pass       
+    def test_first(self):
+        def t(f):
+            self.reset()
+            self.b.squares_l = '212010122'
+            t = self.b.taken()
+            self.assertEqual(f(self.b), True)
+            self.assertEqual(set(self.b.taken()).difference(t).pop() in [3,5], True)
+        t(first)
+        t(eg)
+
+# the tests for the web page themselves are incomplete
+# we need to see that the xml file generated has the appropriate fields, but we don't
+# further, we need to verify that the messages shown in the appropriate fields are in fact correct
+# the only things tested here are response codes. Manual validation has shown that the code is correct, but that isn't always likely to be good enough.
+
+class WebTests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        
+    def test_page_redirect(self):
+        self.assertEqual(self.client.get('/tictactoe/').status_code,302)
+   
+    def test_page_50(self):
+        print self.client.get('/tictactoe/50/').status_code
+
+    def test_ajax_as_normal(self):
+        self.assertEqual(self.client.get('/tictactoe/50/update/', {'m':4}).status_code,404)
+
+    def test_ajax_as_ajax(self):
+        print self.client.get('/tictactoe/50/update/',{'m':4}, HTTP_X_REQUESTED_WITH='XMLHttpRequest').status_code
+
