bcbaf588fec54bc2cee9d08882a349fb8cb5b078
emidln <emidln@gmail.com>
2010-12-13 08:22:02 -0600

    Fixed tests for Board.move

--- tictactoe/tests.py
+++ tictactoe/tests.py
@@ -4,31 +4,33 @@ from exceptions import GameWin, GameDraw, IllegalMove, UndefinedMove
 
 class BoardTests(TestCase):
 
-    def move_normal(self):
+    def test_move_normal(self):
         b = Board()
-        self.assertRaises(GameWin, b.move, 4, 1)
-        self.assertRaises(GameWin, b.move, 0, 2)
+        self.assertEqual(b.move(4, 1), None)
+        self.assertEqual(b.move(0, 2), None)
  
-    def move_win(self):
+    def test_move_win(self):
         b = Board()
-        b.square_l = '110000000'
+        b.squares_l = '110000000'
         b.save()
+        self.assertEqual(b[:],[1,1,0,0,0,0,0,0,0]) 
         self.assertRaises(GameWin, b.move, 2, 1)
         
-    def move_draw(self):
+    def test_move_draw(self):
         b = Board() 
-        b.square_l = '121212220'
+        b.squares_l = '121221110'
         b.save()
-        self.assertRaises(GameDraw, b.move, 8, 1)
+        self.assertRaises(GameDraw, b.move, 8, 2)
 
-    def move_illegal(self):
+    def test_move_illegal(self):
         b = Board()
         b[0] = 1 
         b.save()
         self.assertRaises(IllegalMove, b.move, 0, 1)
     
-    def move_undefined(self):
+    def test_move_undefined(self):
         b = Board()
+        b.save()
         self.assertRaises(UndefinedMove, b.move, 9, 1)
         self.assertRaises(UndefinedMove, b.move, -10, 1)
     
