3c640e8f2432fb99aec5f563842b8fc8198acd95
Brandon Joseph Adams <emidln@gmail.com>
2013-11-14 10:14:24 -0600

    Added module docstring and other style violations

---
+++ tttp/tictactoe/utils.py
@@ -0,0 +1,13 @@
+""" Utility constants used throughout the system.  """
+winning_rows = [
+    range(0, 3),
+    range(3, 6),
+    range(6, 9),
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+COMPUTER = 1
+PLAYER = 2
